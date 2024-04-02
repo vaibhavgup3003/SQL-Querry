141eac31fe82451a220c7ada0787a87584992750
vaibhavgup3003 <211112207@stu.manit.ac.in>
2024-04-02 21:41:00 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://repository-images.githubusercontent.com/275336521/20d38e00-6634-11eb-9d1f-6a5232d0f84f");
+             background-image: url("https://github.com/vaibhavgup3003/SQL-Querry/blob/main/img1.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
